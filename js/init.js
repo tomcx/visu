edd656a87de0dd4b7ff3bb9e039f1da1299dfee9
Thomas Schmidt <tom@darkstar.(none)>
2012-09-16 13:45:58 +0200

    Testhalber Code zur Deaktivierung der Adressleiste eingefügt. Funktioniert zumindest im Chrome nicht.

--- js/init.js
+++ js/init.js
@@ -9,7 +9,7 @@ VISU.init.starten = function() {
     
     //Kontextmenü deaktivieren
     document.body.oncontextmenu = function() {return false};
-    
+
     //Module initialisieren
     VISU.init.bild();
     VISU.init.canvas();
@@ -23,14 +23,28 @@ VISU.init.starten = function() {
     VISU.init.button();
     VISU.init.comm_def();
     
-    try {VISU.comm.cx.logSymbols();} catch(e) {};
+    //Symboltabelle für Fehlersuche in Konsole schreiben
+    //VISU.comm.cx.logSymbols();
+    
+    //Addresszeile deaktivieren
+    function hideAddressBar() {
+        if (document.height <= window.outerHeight + 1) {
+            document.body.style.height = (window.outerHeight + 50) +'px';
+            setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+        } else {
+            setTimeout( function(){ window.scrollTo(0, 1); }, 0 ); 
+        }
+    }
+    
+    hideAddressBar();
+    window.addEventListener("orientationchange", hideAddressBar ); 
     
     //Polling starten
     VISU.anzeig.visinfo.data = 'Warte auf Daten ...';
     VISU.comm.pollZyk1();
-    setTimeout('VISU.comm.pollZyk5()',500); //0,5s versetzt starten
+    setTimeout(VISU.comm.pollZyk5 ,500); //0,5s versetzt starten
     VISU.sonst.warteAufDaten();
-}
+};
 
 window.onload = VISU.init.starten;
  
