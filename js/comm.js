bd946eebaea417a5ade79886082e8bd0e64ee3a6
Thomas Schmidt <tom@darkstar.(none)>
2012-10-09 21:41:37 +0200

    URL geändert.

--- js/comm.js
+++ js/comm.js
@@ -6,14 +6,12 @@
  */
 
 VISU.init.comm = function() {
-  
+    
     VISU.comm = {
         //Webservice erstellen   
         cx: TAME.WebServiceClient.createClient({
-	    serviceUrl: window.location.protocol + "//" + window.location.hostname + '/TcAdsWebService/TcAdsWebService.dll',
-            //serviceUrl: 'http://192.168.1.234/TcAdsWebService/TcAdsWebService.dll',
-            //serviceUrl: 'http://127.0.0.1/TcAdsWebService/TcAdsWebService.dll',
-            //serviceUrl: 'http://192.168.1.2/TcAdsWebService/TcAdsWebService.dll',
+            //serviceUrl: window.location.protocol + "//" + window.location.hostname + '/TcAdsWebService/TcAdsWebService.dll',
+            serviceUrl: window.location.href.replace(/visu\/main\.html/,'/TcAdsWebService/TcAdsWebService.dll'),
             amsNetId: '5.2.52.54.1.1',
             //amsNetId: '192.168.1.2.1.1',
             dataAlign4: true
